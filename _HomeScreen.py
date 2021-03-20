d66773b5136dd253f478c60ef0937875a79cc4b5
yetitodie <80911327+yetitodie@users.noreply.github.com>
2021-03-20 14:30:43 +0530

    Add files via upload

---
+++ _HomeScreen.py
@@ -0,0 +1,44 @@
+from tkinter import *
+from aClock import Clock
+from aWeather import Weather
+from aNews import News
+
+class HomeScreen(Frame):
+
+    def __init__(self, parent):
+        Frame.__init__(self, parent, bg="black")
+        self.topFrame = Frame(self, background = 'black')
+        self.bottomFrame = Frame(self, background = 'black')
+        self.topFrame.pack(side = TOP, fill=BOTH, expand = YES)
+        self.bottomFrame.pack(side = BOTTOM, fill=BOTH, expand = YES)
+       
+        # weather
+        self.weather = Weather(self.topFrame)
+        self.weather.pack(side=LEFT, anchor=N, padx=100, pady=60)
+        #clock
+        self.clock = Clock(self.topFrame)
+        self.clock.pack(side=RIGHT, anchor=N, padx=100, pady=60)
+        # news
+        self.news = News(self.bottomFrame)
+        self.news.pack(side=LEFT, anchor=S, padx=100, pady=60)
+
+
+if __name__ == "__main__": 
+    class MainScreen:
+        def __init__(self):
+            self.tk = Tk()
+            self.tk.configure(background='black')
+            self.frm = Frame(self.tk, background="black")
+            self.frm.pack(side = TOP, fill=BOTH, expand = YES)
+            self.screen = HomeScreen(self.frm)
+            self.screen.pack(side = TOP, fill=BOTH, expand = YES)
+            self.state = False
+            self.tk.bind("<Return>", self.toggle_fullscreen)
+
+        def toggle_fullscreen(self, event=None):
+            self.state = not self.state  # Just toggling the boolean
+            self.tk.attributes("-fullscreen", self.state)
+            return "break"
+
+    w = MainScreen()
+    w.tk.mainloop()
